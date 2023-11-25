fead079b6f298eba1fa1a72d55c529e7eb9db6bd
CyprienGille <>
2023-11-25 11:17:42 +0100

    Allow for pushing phase switch

--- src/phases.rs
+++ src/phases.rs
@@ -11,13 +11,13 @@ impl Plugin for GamePhasePlugin {
 }
 
 fn check_phase(
-    mut query: Query<(&Player, &mut CanMove)>,
+    mut player_query: Query<(&Player, &mut CanMove)>,
     mut game_state: ResMut<GameState>,
     keys: Res<Input<KeyCode>>,
 ) {
-    for (player, mut can_move) in &mut query {
+    for (player, mut can_move) in &mut player_query {
         // if it is a player's turn, allow them to move
-        if game_state.current_player_id == player.id {
+        if game_state.current_player_id == player.id && !game_state.tile_push_phase {
             *can_move = CanMove::Yes;
         } else {
             *can_move = CanMove::No;
@@ -26,5 +26,9 @@ fn check_phase(
     if keys.just_pressed(KeyCode::T) {
         // end turn
         game_state.current_player_id = (game_state.current_player_id + 1) % NUM_PLAYERS;
+        game_state.tile_push_phase = true;
+    }
+    if keys.just_pressed(KeyCode::Return) {
+        game_state.tile_push_phase = false;
     }
 }
