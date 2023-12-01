168e1bcb1b9b9e65b366b191586ee745d69943d2
Cyprien <cyprien.gille@orange.fr>
2023-12-01 13:33:16 +0100

    Add back import

--- src/board_selector.rs
+++ src/board_selector.rs
@@ -1,7 +1,10 @@
 use bevy::prelude::*;
 use once_cell::sync::Lazy;
 
-use crate::{board::Board, boards_repository::BOARD_CLASSIC};
+use crate::{
+    board::Board,
+    boards_repository::{BOARD_0, BOARD_CLASSIC},
+};
 
 #[derive(Resource)]
 pub struct SelectedBoard {
