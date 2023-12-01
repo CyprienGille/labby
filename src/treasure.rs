5000d3643fddbeb142cf24f2256ad43eba75c098
Cyprien <cyprien.gille@orange.fr>
2023-12-01 13:33:08 +0100

    Documentation

--- src/treasure.rs
+++ src/treasure.rs
@@ -7,6 +7,7 @@ use crate::{
     actors::{get_random_pos_on_axis, GridAxis, SpawnPosition},
     board_selector::SelectedBoard,
     movement::{get_max_coords, pos_is_external},
+    player::Player,
     tile::{TileType, TILE_SIZE},
     GameSettings, GridPosition,
 };
@@ -27,14 +28,21 @@ struct TreasureBundle {
 
 #[derive(Resource, Debug, Default)]
 struct TreasureLists {
+    // the to-be-collected treasure list for each player
     lists: HashMap<i32, Vec<i32>>,
 }
 
+#[derive(Resource, Debug, Default)]
+struct CollectedLists {
+    // the collected treasure list for each player
+    lists: HashMap<i32, Vec<i32>>,
+}
 pub struct TreasurePlugin;
 
 impl Plugin for TreasurePlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(TreasureLists { ..default() })
+            .insert_resource(CollectedLists { ..default() })
             .add_systems(Startup, spawn_all_treasures)
             .add_systems(PostStartup, init_treasure_lists)
             .add_systems(Update, move_treasure_with_ext_tile);
@@ -47,6 +55,7 @@ fn spawn_all_treasures(
     selected_board: Res<SelectedBoard>,
     asset_server: Res<AssetServer>,
 ) {
+    // all of the available treasure sprites
     let sprite_paths = read_dir("assets/treasures/")
         .unwrap()
         .filter_map(|e| e.ok())
@@ -54,6 +63,7 @@ fn spawn_all_treasures(
         .map(|s| s.replace("assets/", ""))
         .collect::<Vec<_>>();
 
+    // the number of set treasure positions in the selected board
     let num_set_pos: i32 = selected_board
         .board
         .treasure_positions
@@ -64,6 +74,7 @@ fn spawn_all_treasures(
     let mut all_player_spawns: Vec<GridPosition> = vec![];
     let mut used_pos: Vec<GridPosition> = vec![];
 
+    // collect set player spawns to avoid spawning treasures on top of them
     for spawn_pos in &selected_board.board.spawn_positions {
         match spawn_pos {
             SpawnPosition::Position(grid_pos) => all_player_spawns.push(*grid_pos),
@@ -74,6 +85,7 @@ fn spawn_all_treasures(
     for id in 0..(game_settings.num_players * game_settings.treasures_to_get) {
         let mut current_spawn_pos = SpawnPosition::Any;
 
+        // if there are set positions left
         if id < num_set_pos {
             current_spawn_pos = selected_board.board.treasure_positions[id as usize];
         }
@@ -91,6 +103,7 @@ fn spawn_all_treasures(
                 used_pos.push(GridPosition { x_pos, y_pos });
             }
             SpawnPosition::Any => {
+                // get random unused position
                 let mut x_pos = get_random_pos_on_axis(GridAxis::X, &selected_board);
                 let mut y_pos = get_random_pos_on_axis(GridAxis::Y, &selected_board);
                 while all_player_spawns.contains(&GridPosition { x_pos, y_pos })
@@ -144,7 +157,11 @@ fn init_treasure_lists(
     mut treasure_lists: ResMut<TreasureLists>,
     game_settings: Res<GameSettings>,
 ) {
+    // Init the to-be-collected treasure lists
+
     let mut rng = thread_rng();
+
+    // shuffled list of all of the valid treasure ids
     let mut all_treasure_ids =
         (0..game_settings.num_players * game_settings.treasures_to_get).collect::<Vec<i32>>();
     all_treasure_ids.shuffle(&mut rng);
@@ -153,6 +170,7 @@ fn init_treasure_lists(
         .chunks(game_settings.treasures_to_get.try_into().unwrap())
         .enumerate()
     {
+        // iterate on chunks of size treasures_to_get of the shuffled list of ids
         treasure_lists
             .lists
             .insert(player_id.try_into().unwrap(), chunk.to_vec());
@@ -164,19 +182,46 @@ fn move_treasure_with_ext_tile(
     mut treasures_query: Query<(&mut GridPosition, &mut Transform, &Treasure), Without<TileType>>,
     selected_board: Res<SelectedBoard>,
 ) {
+    // If the external tile had a treasure on it, move that treasure along with the external tile
+
     let (max_x, max_y) = get_max_coords(&selected_board);
 
     for tile_grid_pos in &tiles_query {
         if pos_is_external(tile_grid_pos, max_x, max_y) {
+            // This is the external tile
             for (mut treasure_grid_pos, mut treasure_transform, _treasure) in &mut treasures_query {
                 if pos_is_external(&treasure_grid_pos, max_x, max_y) {
+                    // This is the external treasure
+
                     *treasure_grid_pos = *tile_grid_pos;
+                    // Make it follow the external tile
                     treasure_transform.translation = Vec3::new(
                         tile_grid_pos.x_pos as f32 * TILE_SIZE.x * TREASURE_SCALE.x,
                         tile_grid_pos.y_pos as f32 * TILE_SIZE.y * TREASURE_SCALE.y,
                         2.0,
                     )
                 }
+            }
+        }
+    }
+}
+
+fn collect_treasure(
+    player_query: Query<(&Player, &GridPosition)>,
+    treasure_query: Query<(&Treasure, &GridPosition)>,
+    treasure_lists: ResMut<TreasureLists>,
+    collected_lists: ResMut<CollectedLists>,
+) {
+    for (player, p_grid_pos) in &player_query {
+        for (treasure, t_grid_pos) in &treasure_query {
+            if p_grid_pos == t_grid_pos
+                && treasure_lists
+                    .lists
+                    .get(&player.id)
+                    .unwrap()
+                    .contains(&treasure.id)
+            {
+                todo!()
             }
         }
     }
