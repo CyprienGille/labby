4cb76d4c97587883d99712171ec4a2cc35712994
CyprienGille <>
2023-12-20 23:41:27 +0100

    Add background

--- src/board.rs
+++ src/board.rs
@@ -8,7 +8,9 @@ use crate::phases::GameState;
 use crate::tile::spawn_tile;
 use crate::tile::TileType;
 
-// The general internal board representation
+const GROUND_SIZE: Vec3 = Vec3::new(1600.0, 1600.0, 0.0);
+
+/// The general internal board representation
 #[derive(Debug, Clone)]
 pub struct Board {
     pub tiles: Array2<TileInfo>,
@@ -29,7 +31,8 @@ pub struct BoardPlugin;
 
 impl Plugin for BoardPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::Playing), spawn_board)
+        app.add_systems(Startup, spawn_ground)
+            .add_systems(OnEnter(GameState::Playing), spawn_board)
             .add_systems(OnExit(GameState::Playing), cleanup_board);
     }
 }
@@ -78,5 +81,24 @@ fn spawn_board(
 fn cleanup_board(mut commands: Commands, tiles_query: Query<Entity, With<TileType>>) {
     for entity in &tiles_query {
         commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn spawn_ground(mut commands: Commands, asset_server: Res<AssetServer>) {
+    for x in -30..31 {
+        for y in -30..31 {
+            commands.spawn(SpriteBundle {
+                texture: asset_server.load("ground.png"),
+                transform: Transform {
+                    translation: Vec3::new(
+                        x as f32 * GROUND_SIZE.x,
+                        y as f32 * GROUND_SIZE.y,
+                        -1.0,
+                    ),
+                    ..default()
+                },
+                ..default()
+            });
+        }
     }
 }
